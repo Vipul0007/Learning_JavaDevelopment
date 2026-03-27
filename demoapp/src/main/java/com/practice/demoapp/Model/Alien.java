3c29dfd415915714da81893751bf40d1dcfe7465
Vipul <vipulbansal799@gmail.com>
2026-03-27 23:44:51 +0530

    minor issues resolve

---
+++ demoapp/src/main/java/com/practice/demoapp/Model/Alien.java
@@ -0,0 +1,22 @@
+@Entity
+class Alien{
+    @Id
+    private int aid;
+    private String aname;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+}
